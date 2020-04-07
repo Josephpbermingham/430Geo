4f91d52fe0cf745e4c59a4e1ca8aab048abb3a4d
jraja2 <31863522+jraja2@users.noreply.github.com>
2020-04-07 19:25:45 -0400

    Add files via upload

---
+++ Tags.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newvivo;
+import java.util.*;
+/**
+ *
+ * @author Jazeb R
+ */
+public class Tags {
+    String tagContent;
+    String tagName;
+    String tagColor;
+    
+    public Tags(String name, String content, String color){
+    tagName = name;
+    tagContent = content;
+    tagColor = color;
+    }
+    public String getName(){
+     return tagName;   
+    }
+    public String getContent(){
+        return tagContent;
+    }
+    public String getColor(){
+        return tagColor;
+    }
+    public void setName(String newName){
+        tagName = newName;
+    }
+    public void setContent(String newContent){
+        tagContent = newContent;
+    }
+    public void setColor(String newColor){
+        tagColor = newColor;
+    }
+}
+
